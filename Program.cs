6e3c267cdf96a435d473b47ba4e7f7004ffa7054
Tali04001 <89461076+Tali04001@users.noreply.github.com>
2021-12-10 15:08:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Demo {
+   static void Main() {
+      List<long> list = new List<long> { 11, 5, 3, 3, 6, 12 };
+      foreach(long ele in list){
+         Console.WriteLine(ele);
+      }
+
+      // getting largest element
+      long största = list.AsQueryable().Max();
+
+      // getting smallest element
+      long minsta = list.AsQueryable().Min();
+
+      Console.WriteLine("Smallest number = {0}", minsta);
+      Console.WriteLine("Largest number = {0}", största);
+   }
+}
